1043dbe259a84ed22cb4f9c1197dedaf28940779
Simone Chiusoli <simone.chiusoli2@studio.unibo.it>
2022-06-10 18:17:35 +0200

    added red dots when game is transitive

--- plot.py
+++ plot.py
@@ -1,12 +1,15 @@
 import pickle
 import matplotlib.pyplot as plt
+import math
 
 
-p,intransitiveness = pickle.load(open("data.pkl", "rb"))
-print(intransitiveness.shape)
-print(p.shape)
+p, intransitiveness = pickle.load(open("data.pkl", "rb"))
 # plotting the function intransitiveness(p)
 plt.plot(p, intransitiveness)
-plt.xlabel("probability")
-plt.ylabel("intransitiveness")
+plt.xlabel("Probability")
+plt.ylabel("Intransitiveness")
+plt.title("Penney's Game")
+for n in range(len(p)):
+    if math.isclose(0, intransitiveness[n], abs_tol=0.001):
+        plt.plot(p[n], intransitiveness[n], 'ro')
 plt.show()
