b1c0a14fa059b0b977d98c35dd13bfc251014262
Simone Chiusoli <simone.chiusoli2@studio.unibo.it>
2022-07-01 16:34:16 +0200

    saved image file

--- plot.py
+++ plot.py
@@ -6,7 +6,7 @@ import configparser
 
 configu = configparser.ConfigParser()
 configu.read("config.txt")
-title = configu.get('paths', 'intransitiveness_pic')
+title = configu.get('settings', 'title')
 p, intransitiveness = json.load(open("data.json", "r"))
 # plotting the function intransitiveness(p)
 plt.plot(p, intransitiveness)
@@ -16,4 +16,6 @@ plt.title(title)
 for n in range(len(p)):
     if math.isclose(0, intransitiveness[n], abs_tol=0.001):
         plt.plot(p[n], intransitiveness[n], 'ro')
+plt.savefig('./images/intransitiveness.png')
 plt.show()
+
