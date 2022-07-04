8219405b434ffafe5e7fccdfa0cd60b1e15d6751
Simone Chiusoli <simone.chiusoli2@studio.unibo.it>
2022-07-04 17:40:53 +0200

    added paths for saving plots

--- plot.py
+++ plot.py
@@ -8,6 +8,8 @@ configu = configparser.ConfigParser()
 configu.read("config.txt")
 title = configu.get('settings', 'title')
 title2 = configu.get('settings', 'title2')
+destination1 = configu.get('paths', 'intransitiveness_pic')
+destination2 = configu.get('paths', 'intransitiveness2_pic')
 p, intransitiveness = json.load(open("data.json", "r"))
 # plotting the function intransitiveness(p)
 plt.plot(p, intransitiveness)
@@ -17,7 +19,7 @@ plt.title(title)
 for n in range(len(p)):
     if math.isclose(0, intransitiveness[n], abs_tol=0.01):
         plt.plot(p[n], intransitiveness[n], 'ro')
-plt.savefig('./images/intransitiveness.png')
+plt.savefig(destination1)
 plt.show()
 
 p, intransitiveness2 = json.load(open("data2.json", "r"))
@@ -28,5 +30,5 @@ plt.title(title2)
 for n in range(len(p)):
     if math.isclose(0, intransitiveness2[n], abs_tol=0.01):
         plt.plot(p[n], intransitiveness2[n], 'ro')
-plt.savefig('./images/intransitiveness2.png')
+plt.savefig(destination2)
 plt.show()
